b2cff7c9f5840cbee60a47cd71e7103733a86fee
Ndivhuwo Nyase <65094560+nnyase@users.noreply.github.com>
2023-03-08 12:02:01 +0200

    update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 from setuptools import find_packages, setup
 from typing import List
 
+HYPHEN_E_DOT = '-e .'
 
 def get_requirements(file_path:str)->List[str]:
     ''' 
@@ -10,6 +11,11 @@ def get_requirements(file_path:str)->List[str]:
     with open(file_path) as file_obj:
         requirements=file_obj.readlines()
         requirements = [req.replace("\n","") for req in requirements] 
+        
+        if HYPHEN_E_DOT in requirements:
+            requirements.remove(HYPHEN_E_DOT)
+
+    return requirements
     
 
 setup( 
@@ -18,7 +24,7 @@ version = '0.0.1',
 author='Ndivhuwo',
 author_email='n.nyase@gmail.com',
 packages=find_packages(),
-install_requires =get_requirements('reqirements.txt')  
+install_requires =get_requirements('requirements.txt')  
 
 
 
